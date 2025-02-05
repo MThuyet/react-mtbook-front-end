0f948533fa81f310ceefec344bd115a4de90106e
psychisch <mthuyet294@gmail.com>
2025-02-05 18:58:59 +0700

    setup react router && demo how to use #23

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,7 +1,18 @@
+import { Outlet } from 'react-router-dom'
+import AppHeader from './components/layout/app.header'
+
 function Layout() {
 
 	return (
-		<div>Hello World with HoiDanIT update</div>
+
+		<div>
+			<AppHeader />
+
+			{/* muốn chia sẻ component nào thì sử dụng Outlet */}
+			<Outlet />
+
+		</div>
+
 	)
 }
 
