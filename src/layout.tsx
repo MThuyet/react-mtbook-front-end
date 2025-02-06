de3abca78cff92d6232f409ad67d40b081e1743d
psychisch <mthuyet294@gmail.com>
2025-02-06 22:12:23 +0700

    customize loading data

--- src/layout.tsx
+++ src/layout.tsx
@@ -3,16 +3,18 @@ import AppHeader from './components/layout/app.header'
 import { useEffect } from 'react'
 import { fetchAccountAPI } from 'services/api'
 import { useCurrentApp } from './components/context/app.context'
+import PacmanLoader from 'react-spinners/PacmanLoader'
 
 function Layout() {
 
-	const { setUser, isAppLoading, setisAppLoading } = useCurrentApp();
+	const { setUser, isAppLoading, setisAppLoading, setIsAuthenticated } = useCurrentApp();
 
 	useEffect(() => {
 		const fetchAccount = async () => {
 			const res = await fetchAccountAPI();
 			if (res.data) {
 				setUser(res.data.user);
+				setIsAuthenticated(true);
 			}
 			setisAppLoading(false)
 		}
@@ -21,15 +23,19 @@ function Layout() {
 	}, [])
 
 	return (
+		<>
+			{isAppLoading === false ? <div>
+				<AppHeader />
+				{/* muốn chia sẻ component nào thì sử dụng Outlet */}
+				<Outlet />
+			</div> : <div style={{ position: "fixed", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}>
+				<PacmanLoader color='#36d6b4' size={50} />
+				<h2 style={{ marginTop: "1rem" }}>Loading...</h2>
+			</div>}
 
-		<div>
-			<AppHeader />
 
-			{/* muốn chia sẻ component nào thì sử dụng Outlet */}
-			<Outlet />
 
-		</div>
-
+		</>
 	)
 }
 
--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,7 @@ export const loginAPI = (username: string, password: string) => {
 	const urlBackend = '/api/v1/auth/login';
 	return axios.post<IBackendRes<ILogin>>(urlBackend, { username, password }, {
 		headers: {
-			delay: 3000
+			delay: 2000
 		}
 	});
 }
@@ -16,5 +16,9 @@ export const registerAPI = (fullName: string, email: string, password: string, p
 
 export const fetchAccountAPI = () => {
 	const urlBackend = '/api/v1/auth/account';
-	return axios.get<IBackendRes<IFetchAccount>>(urlBackend);
+	return axios.get<IBackendRes<IFetchAccount>>(urlBackend, {
+		headers: {
+			delay: 2000
+		}
+	});
 }
