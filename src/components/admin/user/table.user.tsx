8a06322a36b9248ce77209a1634947ef524c058f
psychisch <mthuyet294@gmail.com>
2025-02-07 12:34:51 +0700

    customize table user

--- src/components/admin/user/table.user.tsx
+++ src/components/admin/user/table.user.tsx
@@ -3,7 +3,8 @@ import { PlusOutlined } from '@ant-design/icons';
 import type { ActionType, ProColumns } from '@ant-design/pro-components';
 import { ProTable, TableDropdown } from '@ant-design/pro-components';
 import { Button, Space, Tag } from 'antd';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
 
 const columns: ProColumns<IUserTable>[] = [
 	{
@@ -12,8 +13,14 @@ const columns: ProColumns<IUserTable>[] = [
 		width: 48,
 	},
 	{
-		title: '_id',
+		title: 'ID',
 		dataIndex: '_id',
+		hideInSearch: true,
+		render(dom, entity, index, action, schema) {
+			return (
+				<a href="#">{entity._id}</a>
+			)
+		},
 	},
 	{
 		title: 'Full name',
@@ -22,15 +29,35 @@ const columns: ProColumns<IUserTable>[] = [
 	{
 		title: 'Email',
 		dataIndex: 'email',
+		copyable: true,
 	},
 	{
 		title: 'Created at',
 		dataIndex: 'createdAt',
 	},
+	{
+		title: 'Actions',
+		hideInSearch: true,
+		render: () => {
+			return (
+				<>
+					<Button style={{ marginRight: '10px', borderColor: 'rgb(231, 112, 13' }} ><EditOutlined style={{ color: 'rgb(231, 112, 13' }} /></Button>
+					<Button style={{ borderColor: '#f5222d' }}><DeleteOutlined style={{ color: '#f5222d' }} /></Button>
+				</>
+			);
+		}
+	},
 ];
 
 const TableUser = () => {
 	const actionRef = useRef<ActionType>();
+	const [meta, setMete] = useState({
+		current: 1,
+		pageSize: 5,
+		pages: 0,
+		total: 0
+	});
+
 	return (
 		<>
 			<ProTable<IUserTable>
@@ -38,21 +65,34 @@ const TableUser = () => {
 				actionRef={actionRef}
 				cardBordered
 				request={async (params, sort, filter) => {
-					console.log(sort, filter);
+
 					const res = await getUsersAPI();
+
+					if (res.data) {
+						setMete(res.data.meta);
+					}
 
 					return {
 						data: res.data?.result,
-						"page": 1,
-						"success": true,
-						"total": res.data?.meta.total
+						page: 1,
+						success: true,
+						total: res.data?.meta.total
 					}
 				}}
-				rowKey="id"
-				pagination={{
-					pageSize: 5,
-					onChange: (page) => console.log(page),
-				}}
+				rowKey="_id"
+				pagination={
+					{
+						current: meta.current,
+						pageSize: meta.pageSize,
+						showSizeChanger: true,
+						total: meta.total,
+						showTotal(total, range) {
+							return (
+								<div> {range[0]} - {range[1]} / {total} Users</div>
+							)
+						},
+					}
+				}
 				headerTitle="Table user"
 				toolBarRender={() => [
 					<Button
