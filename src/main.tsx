0c18da07ac652de02c2d990a217e538c101e97f9
psychisch <mthuyet294@gmail.com>
2025-02-07 10:39:17 +0700

    use antd pro to design table manage user

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Layout from '@/layout';
 import {
-  createBrowserRouter,
-  RouterProvider,
+	createBrowserRouter,
+	RouterProvider,
 } from "react-router-dom";
 import BookPage from 'pages/client/book';
 import AboutPage from 'pages/client/about';
@@ -11,7 +11,7 @@ import LoginPage from 'pages/client/auth/login';
 import RegisterPage from 'pages/client/auth/register';
 import 'styles/global.scss'
 import HomePage from 'pages/client/home';
-import { App } from 'antd';
+import { App, ConfigProvider } from 'antd';
 import { AppProvider } from 'components/context/app.context';
 import ProtectedRoute from '@/components/auth';
 import DashBoardPage from 'pages/admin/dashboard';
@@ -19,98 +19,101 @@ import ManageBookPage from 'pages/admin/manage.book';
 import ManageOrderPage from 'pages/admin/manage.order';
 import ManageUserPage from 'pages/admin/manage.user';
 import LayoutAdmin from 'components/layout/layout.admin';
+import enUS from 'antd/locale/en_US';
+import viVN from 'antd/locale/vi_VN';
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        index: true,
-        element: <HomePage />
-      },
-      {
-        path: "/book",
-        element: <BookPage />,
-      },
-      {
-        path: "/about",
-        element: <AboutPage />,
-      },
-      {
-        path: "/checkout",
-        element: (
-          <ProtectedRoute>
-            <div>checkout page</div>
-          </ProtectedRoute>
-        ),
-      }
-    ]
-  },
-  {
-    path: "admin",
-    element: <LayoutAdmin />,
-    children: [
-      {
-        index: true,
-        element: (
-          <ProtectedRoute>
-            <DashBoardPage />
-          </ProtectedRoute>
-        )
-      },
-      {
-        path: "book",
-        element: (
-          <ProtectedRoute>
-            <ManageBookPage />
-          </ProtectedRoute>
-        )
-      },
-      {
-        path: "order",
-        element: (
-          <ProtectedRoute>
-            <ManageOrderPage />
-          </ProtectedRoute>
-        )
-      },
-      {
-        path: "user",
-        element: (
-          <ProtectedRoute>
-            <ManageUserPage />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: "/admin",
-        element: (
-          <ProtectedRoute>
-            <div>admin page</div>
-          </ProtectedRoute>
-        ),
-      },
+	{
+		path: "/",
+		element: <Layout />,
+		children: [
+			{
+				index: true,
+				element: <HomePage />
+			},
+			{
+				path: "/book",
+				element: <BookPage />,
+			},
+			{
+				path: "/about",
+				element: <AboutPage />,
+			},
+			{
+				path: "/checkout",
+				element: (
+					<ProtectedRoute>
+						<div>checkout page</div>
+					</ProtectedRoute>
+				),
+			}
+		]
+	},
+	{
+		path: "admin",
+		element: <LayoutAdmin />,
+		children: [
+			{
+				index: true,
+				element: (
+					<ProtectedRoute>
+						<DashBoardPage />
+					</ProtectedRoute>
+				)
+			},
+			{
+				path: "book",
+				element: (
+					<ProtectedRoute>
+						<ManageBookPage />
+					</ProtectedRoute>
+				)
+			},
+			{
+				path: "order",
+				element: (
+					<ProtectedRoute>
+						<ManageOrderPage />
+					</ProtectedRoute>
+				)
+			},
+			{
+				path: "user",
+				element: (
+					<ProtectedRoute>
+						<ManageUserPage />
+					</ProtectedRoute>
+				),
+			},
+			{
+				path: "/admin",
+				element: (
+					<ProtectedRoute>
+						<div>admin page</div>
+					</ProtectedRoute>
+				),
+			},
 
-    ]
-  },
-  {
-    path: "/login",
-    element: <LoginPage />,
-  },
-  {
-    path: "/register",
-    element: <RegisterPage />,
-  },
+		]
+	},
+	{
+		path: "/login",
+		element: <LoginPage />,
+	},
+	{
+		path: "/register",
+		element: <RegisterPage />,
+	},
 
 ]);
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App>
-      <AppProvider>
-        <RouterProvider router={router} />
-      </AppProvider>
-    </App>
-  </StrictMode>,
+	<StrictMode>
+		<App>
+			<AppProvider>
+				<ConfigProvider locale={enUS}> <RouterProvider router={router} /></ConfigProvider>
+
+			</AppProvider>
+		</App>
+	</StrictMode>,
 )
