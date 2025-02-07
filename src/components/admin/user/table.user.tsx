c71c93e708e0a16059afc1b5f47cc9ccb8d2a5d2
psychisch <mthuyet294@gmail.com>
2025-02-07 15:10:07 +0700

    custom pagination table user and call api

--- src/components/admin/user/table.user.tsx
+++ src/components/admin/user/table.user.tsx
@@ -65,8 +65,9 @@ const TableUser = () => {
 				actionRef={actionRef}
 				cardBordered
 				request={async (params, sort, filter) => {
+					console.log(params, sort, filter)
 
-					const res = await getUsersAPI();
+					const res = await getUsersAPI(params?.current ?? 1, params?.pageSize ?? 5);
 
 					if (res.data) {
 						setMete(res.data.meta);
