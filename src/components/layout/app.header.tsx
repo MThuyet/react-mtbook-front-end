eccd424e03a2fb260ea04bb011dbadf620ff8542
MThuyet <mthuyet294@gmail.com>
2025-02-17 23:19:36 +0700

    update current user

--- src/components/layout/app.header.tsx
+++ src/components/layout/app.header.tsx
@@ -9,12 +9,13 @@ import './app.header.scss';
 import { Link } from 'react-router-dom';
 import { useCurrentApp } from 'components/context/app.context';
 import { logoutAPI } from '@/services/api';
+import ManageAccount from '../client/account';
 
 const AppHeader = (props: any) => {
-	const { carts } = useCurrentApp();
 	const [openDrawer, setOpenDrawer] = useState(false);
+	const [openManageAccount, setOpenManageAccount] = useState<boolean>(false);
 
-	const { isAuthenticated, user, setUser, setIsAuthenticated } = useCurrentApp();
+	const { isAuthenticated, user, setUser, setIsAuthenticated, carts } = useCurrentApp();
 
 	const { message } = App.useApp();
 
@@ -35,7 +36,7 @@ const AppHeader = (props: any) => {
 		{
 			label: <label
 				style={{ cursor: 'pointer' }}
-				onClick={() => alert("me")}
+				onClick={() => setOpenManageAccount(true)}
 			>Quản lý tài khoản</label>,
 			key: 'account',
 		},
@@ -168,6 +169,11 @@ const AppHeader = (props: any) => {
 				<p onClick={() => handleLogout()}>Đăng xuất</p>
 				<Divider />
 			</Drawer>
+
+			<ManageAccount
+				isModalOpen={openManageAccount}
+				setIsModalOpen={setOpenManageAccount}
+			/>
 
 		</>
 	)
