c4c515ed99552a274e50a4f71670d4316f5d8d44
MThuyet <mthuyet294@gmail.com>
2025-02-18 21:24:43 +0700

    refresh token when access_token expire

--- src/services/axios.customize.ts
+++ src/services/axios.customize.ts
@@ -6,6 +6,15 @@ const createInstanceAxios = (baseURL: string) => {
 		withCredentials: true
 	});
 
+	const handleRefreshToken = async () => {
+		const res = await instance.get('/api/v1/auth/refresh');
+		if (res.data) {
+			return res.data.access_token;
+		} else {
+			return null
+		}
+	}
+
 	// Add a request interceptor
 	instance.interceptors.request.use(function (config) {
 		// Do something before request is sent
@@ -27,12 +36,23 @@ const createInstanceAxios = (baseURL: string) => {
 			return response.data;
 		}
 
-	}, function (error) {
+	}, async function (error) {
 		// Any status codes that falls outside the range of 2xx cause this function to trigger
 		// Do something with response error
-		if (error.response && error.response.data) {
-			return error.response.data;
+		if (error.config && error.response && +error.response.status === 401) {
+			const access_token = await handleRefreshToken();
+			if (access_token) {
+				error.config.headers['Authorization'] = `Bearer ${access_token}`;
+				localStorage.setItem('access_token', access_token);
+				return instance.request(error.config);
+			}
 		}
+
+		if (error && error.response && error.response.data) {
+			return error.response.data
+		}
+
+		return Promise.reject(error);
 	});
 
 	return instance;
